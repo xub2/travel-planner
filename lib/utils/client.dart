d463cffd7738941abe17b636754d7a58bc96b7ea
xub2 <victory62808@gmail.com>
2024-11-22 23:10:08 +0900

    11/22 client 파일 전체 주석 처리 (불필요)

--- lib/utils/client.dart
+++ lib/utils/client.dart
@@ -1,43 +1,45 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-//
-// class TravelData {
-//   String? destination;
-//   DateTime? departureDate;
-//   DateTime? arrivalDate;
-//   double? budget;
-//   List<String> styles = [];
-//
-//   TravelData({
-//     this.destination,
-//     this.departureDate,
-//     this.arrivalDate,
-//     this.budget,
-//     required this.styles,
-//   });
-//
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'destination': destination,
-//       'departureDate': departureDate?.toIso8601String(),
-//       'arrivalDate': arrivalDate?.toIso8601String(),
-//       'budget': budget,
-//       'styles': styles,
-//     };
-//   }
-//
-//   Future<void> sendToServer() async {
-//     final url = Uri.parse('http://127.0.0.1:8000/plan_trip');
-//     final headers = {"Content-Type": "application/json"};
-//     final body = jsonEncode(toJson());
-//
-//     final response = await http.post(url, headers: headers, body: body);
-//     if (response.statusCode == 200) {
-//       print('Server Response: ${jsonDecode(response.body)['message']}');
-//     } else {
-//       print('Failed to send data: ${response.statusCode}');
-//     }
-//   }
-// }
-//
-//
+// 필요는 없지만 혹시 몰라 주석으로 처리
+/*
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TravelData {
+  String? destination;
+  DateTime? departureDate;
+  DateTime? arrivalDate;
+  double? budget;
+  List<String> styles = [];
+
+  TravelData({
+    this.destination,
+    this.departureDate,
+    this.arrivalDate,
+    this.budget,
+    required this.styles,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'destination': destination,
+      'departureDate': departureDate?.toIso8601String(),
+      'arrivalDate': arrivalDate?.toIso8601String(),
+      'budget': budget,
+      'styles': styles,
+    };
+  }
+
+  Future<void> sendToServer() async {
+    final url = Uri.parse('http://127.0.0.1:8000/plan_trip');
+    final headers = {"Content-Type": "application/json"};
+    final body = jsonEncode(toJson());
+
+    final response = await http.post(url, headers: headers, body: body);
+    if (response.statusCode == 200) {
+      print('Server Response: ${jsonDecode(response.body)['message']}');
+    } else {
+      print('Failed to send data: ${response.statusCode}');
+    }
+  }
+}
+*/
+
